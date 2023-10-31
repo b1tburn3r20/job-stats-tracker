d5b744335b63ce48fcd942381314dfdc5c413ad4
Alejandro Bermudez <alejandrojosuebermudez@gmail.com>
2023-10-31 14:20:28 -0500

    added chart types and animations

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const jobsHeardBackFromInDB = ref(database, "jobsHeardBackFrom")
 const jobsRejectedFromInDB = ref(database, "jobsRejectedFrom")
 const jobsInterviewedForInDB = ref(database, "jobsInterviewedFor")
 
-
 const appliedForm = document.getElementById('applied')
 const heardBackForm = document.getElementById('heard-back')
 const rejectedForm = document.getElementById('rejected')
@@ -36,13 +35,15 @@ const submitRejectedInput = document.getElementById('submit-rejected-input')
 const submitInterviewedButton = document.getElementById('submit-interviewed-button')
 const submitInterviewedInput = document.getElementById('submit-interviewed-input')
 
-
+const noticeIcon = document.getElementById('notice-icon')
+const barsIcon = document.getElementById('bars-icon')
+const introText = document.getElementById('intro')
+const formSelector = document.getElementById('form-selector')
 
 onValue(jobStatsInDB, function(snapshot){
     if(snapshot.exists()){
         clearList(jobsAppliedTo)
         let jobsArray = Object.entries(snapshot.val())
-        jobsArray.reverse()
         for(const job of jobsArray){
             returnDataFromDB(job, jobStatsInDB, jobsAppliedTo);
         }
@@ -52,13 +53,12 @@ onValue(jobStatsInDB, function(snapshot){
         noJobsMessage.textContent = "No jobs applied for or any stats yet"
         jobsAppliedTo.appendChild(noJobsMessage)
     }
-  });
+});
   
-  onValue(jobsHeardBackFromInDB, function(snapshot){
+onValue(jobsHeardBackFromInDB, function(snapshot){
     if(snapshot.exists()){
         clearList(jobsHeardBackFrom)
         let jobsArray = Object.entries(snapshot.val())
-        jobsArray.reverse()
         for(const job of jobsArray){
             returnDataFromDB(job, jobsHeardBackFromInDB, jobsHeardBackFrom);
         }
@@ -68,13 +68,12 @@ onValue(jobStatsInDB, function(snapshot){
         noJobsMessage.textContent = "No jobs heard back from or any stats yet"
         jobsHeardBackFrom.appendChild(noJobsMessage)
     }
-  });
+});
   
-  onValue(jobsRejectedFromInDB, function(snapshot){
+onValue(jobsRejectedFromInDB, function(snapshot){
     if(snapshot.exists()){
         clearList(jobsRejectedFrom)
         let jobsArray = Object.entries(snapshot.val())
-        jobsArray.reverse()
         for(const job of jobsArray){
             returnDataFromDB(job, jobsRejectedFromInDB, jobsRejectedFrom);
         }
@@ -84,13 +83,12 @@ onValue(jobStatsInDB, function(snapshot){
         noJobsMessage.textContent = "No jobs rejected from or any stats yet"
         jobsRejectedFrom.appendChild(noJobsMessage)
     }
-  });
+});
   
-  onValue(jobsInterviewedForInDB, function(snapshot){
+onValue(jobsInterviewedForInDB, function(snapshot){
     if(snapshot.exists()){
         clearList(jobsInterviewedFor)
         let jobsArray = Object.entries(snapshot.val())
-        jobsArray.reverse()
         for(const job of jobsArray){
             returnDataFromDB(job, jobsInterviewedForInDB, jobsInterviewedFor);
         }
@@ -100,9 +98,9 @@ onValue(jobStatsInDB, function(snapshot){
         noJobsMessage.textContent = "No jobs interviewed for or any stats yet"
         jobsInterviewedFor.appendChild(noJobsMessage)
     }
-  });
+});
 
-  onValue(jobStatsInDB, function(snapshot){
+onValue(jobStatsInDB, function(snapshot){
     Promise.all([
         getCountForChartFromDB(jobStatsInDB),
         getCountForChartFromDB(jobsHeardBackFromInDB),
@@ -147,7 +145,7 @@ onValue(jobsInterviewedForInDB, function(snapshot){
 });
 
 
-  function getCountFromDB(databaseRef, dbArea){
+function getCountFromDB(databaseRef, dbArea){
     onValue(databaseRef, function(snapshot){
         if(snapshot.exists()){
             let data = snapshot.val();
@@ -186,30 +184,52 @@ function getCountForChartFromDB(databaseRef){
     });
  }
  
-
-
  function toggleChildrenVisibility(dbArea){
     let children = dbArea.querySelectorAll('.database-list-item')
     children.forEach(child => {
         if (child.style.display !== "list-item"){
-            child.style.display = 'list-item'
+            anime({
+                targets: child,
+                opacity: [0,1],
+                duration: 500,
+                easing: 'easeOutExpo',
+                begin: function(anim) {
+                    child.style.display = 'list-item';
+                }
+            });
         }
         else {
-            child.style.display = "none"
+            anime({
+                targets: child,
+                opacity: [1,0],
+                duration: 500,
+                easing: 'easeOutExpo',
+                complete: function(anim) {
+                    child.style.display = 'none';
+                }
+            });
         }
     })
- }
-
-function toggleFormDisplay(form, button){
-    form.style.display = 'none'
-    button.addEventListener('click', function(){
-        if(form.style.display != "none"){
-            form.style.display ="none"
-        }
-        else(form.style.display = 'flex')
-    })
-    
 }
+
+
+ function toggleFormDisplay(form, button){
+    button.addEventListener('click', function(){
+        if(form.classList.contains('show')){
+            form.classList.remove('show');
+            setTimeout(function() {
+                form.style.display = 'none';
+            }, 500);
+        }
+        else{
+            form.style.display = 'flex';
+            setTimeout(function() {
+                form.classList.add('show');
+            }, 0);
+        }
+    })
+}
+
 
 function clearList(listArea){
     listArea.innerHTML = ""
@@ -223,22 +243,73 @@ function returnDataFromDB(itemValue, databaseRef, dbArea){
     newElement.textContent = itemValue[1]
     dbArea.appendChild(newElement)
 }
+
+
+document.getElementById('chart-pie-icon').addEventListener('click', function() {
+    changeChartType(chartTypePie, jobStatsInDB);
+    location.reload();
+});
+
+document.getElementById('chart-simple-icon').addEventListener('click', function() {
+    changeChartType(chartTypeBar, jobsHeardBackFromInDB);
+    location.reload();
+});
+
+document.getElementById('chart-circle-dot-icon').addEventListener('click', function() {
+    changeChartType(chartTypeDonut, jobsRejectedFromInDB);
+    location.reload();
+});
+
+document.getElementById('chart-layer-group-icon').addEventListener('click', function() {
+    changeChartType(chartTypePolarArea, jobsInterviewedForInDB);
+    location.reload();
+});
+
+
+
+
+function changeChartType(type, data) {
+    myPieChart.destroy();
+    myPieChart = new Chart(ctx, {
+        type: type,
+        data: chartData,
+    });
+    localStorage.setItem('lastChartType', type);
+    getCountForChartFromDB(data).then(newData => {
+        updatePieChart(myPieChart, newData);
+        myPieChart.update();
+    });
+}
+
+
+
+
+// On page load, set the chart type to the last used type
+document.addEventListener('DOMContentLoaded', function() {
+    const lastChartType = localStorage.getItem('lastChartType');
+    if (lastChartType) {
+        changeChartType(lastChartType);
+    }
+});
+
 function updatePieChart(myPieChart, data) {
     var chartData = {
         labels: ["Applied", "Heard Back", "Rejected", "Interviewed"],
         datasets: [
             {
+                label: "Hide Chart",
                 data: data,
                 backgroundColor: ["#F8B195", "#F67280", "#C06C84", "#6C5B7B"],
             },
         ],
     };
-
     myPieChart.data = chartData;
+    myPieChart.options.plugins.legend.display = false;
     myPieChart.update();
-}
-var ctx = document.getElementById("myPieChart").getContext("2d");
+ }
 
+
+var ctx = document.getElementById("myPieChart").getContext("2d");
 var chartData = {
     labels: ["Applied", "Heard Back", "Rejected", "Interviewed"],
     datasets: [
@@ -247,15 +318,18 @@ var chartData = {
             backgroundColor: ["#F8B195", "#F67280", "#C06C84", "#6C5B7B"],
         },
     ],
+    
 };
 
 var myPieChart = new Chart(ctx, {
-    type: "bar",
+    type: localStorage.getItem('lastChartType'),
     data: chartData,
 });
 
-
-
+const chartTypeBar = "bar"
+const chartTypePie = "pie"
+const chartTypeDonut = "doughnut"
+const chartTypePolarArea = "polarArea"
 
 function addButtonClickListener(button, inputField, databaseRef){
     button.addEventListener('click', function(){
@@ -266,6 +340,52 @@ function addButtonClickListener(button, inputField, databaseRef){
         // clearInput(inputField)
     })
 }
+
+// noticeIcon.addEventListener('click', function(){
+//     if(introText.style.display !== 'flex'){
+//         anime({
+//             targets: introText,
+//             opacity: [0,1],
+//             duration: 6000,
+//             begin: function(anim) {
+//                 introText.style.display = 'flex';
+//             }
+//         });
+//     }
+//     else {
+//         anime({
+//             targets: introText,
+//             opacity: [1,0],
+//             duration: 500,
+//             complete: function(anim) {
+//                 introText.style.display = 'none';
+//             }
+//         });
+//     }
+// })
+
+barsIcon.addEventListener('click', function(){
+    if(formSelector.style.display !== 'flex'){
+        anime({
+            targets: formSelector,
+            opacity: [0,1],
+            duration: 6000,
+            begin: function(anim) {
+                formSelector.style.display = 'flex';
+            }
+        });
+    }
+    else {
+        anime({
+            targets: formSelector,
+            opacity: [1,0],
+            duration: 500,
+            complete: function(anim) {
+                formSelector.style.display = 'none';
+            }
+        });
+    }
+})
 
 
 addButtonClickListener(submitAppliedButton, submitAppliedInput, jobStatsInDB)
