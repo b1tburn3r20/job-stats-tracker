9ed108ea974fd527c2f90678297ca76347509bce
Alejandro Bermudez <alejandrojosuebermudez@gmail.com>
2023-10-30 20:51:51 -0500

    added proper toggling features and better layout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove,} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
 } 
@@ -22,7 +22,7 @@ const heardBackFormButton = document.getElementById('heard-back-form-button')
 const rejectedFormButton = document.getElementById('rejected-form-button')
 const interviewedFormButton = document.getElementById('interviewed-form-button')
 
-const jobsAppliedFor = document.getElementById('jobs-applied-for')
+const jobsAppliedTo = document.getElementById('jobs-applied-to')
 const jobsHeardBackFrom = document.getElementById('jobs-heard-back-from')
 const jobsRejectedFrom = document.getElementById('jobs-rejected-from')
 const jobsInterviewedFor = document.getElementById('jobs-interviewed-for')
@@ -31,8 +31,114 @@ const submitAppliedButton = document.getElementById('submit-applied')
 const submitAppliedInput = document.getElementById('submit-applied-input')
 const submitHeardBackButton = document.getElementById('submit-heard-back-button')
 const submitHeardBackInput = document.getElementById('submit-heard-back-input')
+const submitRejectedButton = document.getElementById('submit-rejected-button')
+const submitRejectedInput = document.getElementById('submit-rejected-input')
+const submitInterviewedButton = document.getElementById('submit-interviewed-button')
+const submitInterviewedInput = document.getElementById('submit-interviewed-input')
 
-const jobsAppliedTo = document.getElementById('jobs-applied-to')
+
+
+onValue(jobStatsInDB, function(snapshot){
+    if(snapshot.exists()){
+        clearList(jobsAppliedTo)
+        let jobsArray = Object.entries(snapshot.val())
+        jobsArray.reverse()
+        for(const job of jobsArray){
+            returnDataFromDB(job, jobStatsInDB, jobsAppliedTo);
+        }
+    }
+    else{
+        let noJobsMessage = document.createElement('p')
+        noJobsMessage.textContent = "No jobs applied for or any stats yet"
+        jobsAppliedTo.appendChild(noJobsMessage)
+    }
+  });
+  
+  onValue(jobsHeardBackFromInDB, function(snapshot){
+    if(snapshot.exists()){
+        clearList(jobsHeardBackFrom)
+        let jobsArray = Object.entries(snapshot.val())
+        jobsArray.reverse()
+        for(const job of jobsArray){
+            returnDataFromDB(job, jobsHeardBackFromInDB, jobsHeardBackFrom);
+        }
+    }
+    else{
+        let noJobsMessage = document.createElement('p')
+        noJobsMessage.textContent = "No jobs heard back from or any stats yet"
+        jobsHeardBackFrom.appendChild(noJobsMessage)
+    }
+  });
+  
+  onValue(jobsRejectedFromInDB, function(snapshot){
+    if(snapshot.exists()){
+        clearList(jobsRejectedFrom)
+        let jobsArray = Object.entries(snapshot.val())
+        jobsArray.reverse()
+        for(const job of jobsArray){
+            returnDataFromDB(job, jobsRejectedFromInDB, jobsRejectedFrom);
+        }
+    }
+    else{
+        let noJobsMessage = document.createElement('p')
+        noJobsMessage.textContent = "No jobs rejected from or any stats yet"
+        jobsRejectedFrom.appendChild(noJobsMessage)
+    }
+  });
+  
+  onValue(jobsInterviewedForInDB, function(snapshot){
+    if(snapshot.exists()){
+        clearList(jobsInterviewedFor)
+        let jobsArray = Object.entries(snapshot.val())
+        jobsArray.reverse()
+        for(const job of jobsArray){
+            returnDataFromDB(job, jobsInterviewedForInDB, jobsInterviewedFor);
+        }
+    }
+    else{
+        let noJobsMessage = document.createElement('p')
+        noJobsMessage.textContent = "No jobs interviewed for or any stats yet"
+        jobsInterviewedFor.appendChild(noJobsMessage)
+    }
+  });
+
+
+  function getCountFromDB(databaseRef, dbArea){
+    onValue(databaseRef, function(snapshot){
+        if(snapshot.exists()){
+            let data = snapshot.val();
+            let count = Object.keys(data).length
+            let newElement = document.createElement('h3')
+            newElement.textContent = count
+            console.log(count);
+            dbArea.prepend(newElement)
+            newElement.addEventListener('click', function(){
+                toggleChildrenVisibility(dbArea);
+             });
+            return count;
+        }
+        else{
+            console.log("No data in the database reference");
+            let newElement = document.createElement('h3')
+            newElement.textContent = 0
+            dbArea.appendChild(newElement)
+            return 0;
+        }
+    });
+}
+
+
+ function toggleChildrenVisibility(dbArea){
+    let children = dbArea.querySelectorAll('.database-list-item')
+    children.forEach(child => {
+        if (child.style.display !== "flex"){
+            child.style.display = 'flex'
+        }
+        else {
+            child.style.display = "none"
+        }
+    })
+ }
 
 function toggleFormDisplay(form, button){
     form.style.display = 'none'
@@ -49,77 +155,11 @@ function clearList(listArea){
     listArea.innerHTML = ""
 }
 
-toggleFormDisplay(appliedForm, appliedFormButton)
-toggleFormDisplay(heardBackForm, heardBackFormButton)
-toggleFormDisplay(rejectedForm, rejectedFormButton)
-toggleFormDisplay(interviewForm, interviewedFormButton)
-
-
-
-onValue(jobStatsInDB, function(snapshot){
-    if(snapshot.exists()){
-        let jobsArray = Object.entries(snapshot.val())
-        jobsArray.reverse()
-        for(const job of jobsArray){
-            returnDataFromDB(job, jobStatsInDB, jobsAppliedTo);
-        }
-    }
-    else{
-        let noJobsMessage = document.createElement('p')
-        noJobsMessage.textContent = "No jobs applied for or any stats yet"
-        jobsAppliedTo.appendChild(noJobsMessage)
-    }
-  });
-  
-  onValue(jobsHeardBackFromInDB, function(snapshot){
-    if(snapshot.exists()){
-        let jobsArray = Object.entries(snapshot.val())
-        jobsArray.reverse()
-        for(const job of jobsArray){
-            returnDataFromDB(job, jobsHeardBackFromInDB, jobsHeardBackFrom);
-        }
-    }
-    else{
-        let noJobsMessage = document.createElement('p')
-        noJobsMessage.textContent = "No jobs heard back from or any stats yet"
-        jobsHeardBackFrom.appendChild(noJobsMessage)
-    }
-  });
-  
-  onValue(jobsRejectedFromInDB, function(snapshot){
-    if(snapshot.exists()){
-        let jobsArray = Object.entries(snapshot.val())
-        jobsArray.reverse()
-        for(const job of jobsArray){
-            returnDataFromDB(job, jobsRejectedFromInDB, jobsRejectedFrom);
-        }
-    }
-    else{
-        let noJobsMessage = document.createElement('p')
-        noJobsMessage.textContent = "No jobs rejected from or any stats yet"
-        jobsRejectedFrom.appendChild(noJobsMessage)
-    }
-  });
-  
-  onValue(jobsInterviewedForInDB, function(snapshot){
-    if(snapshot.exists()){
-        let jobsArray = Object.entries(snapshot.val())
-        jobsArray.reverse()
-        for(const job of jobsArray){
-            returnDataFromDB(job, jobsInterviewedForInDB, jobsInterviewedFor);
-        }
-    }
-    else{
-        let noJobsMessage = document.createElement('p')
-        noJobsMessage.textContent = "No jobs interviewed for or any stats yet"
-        jobsInterviewedFor.appendChild(noJobsMessage)
-    }
-  });
-  
 // function clearInput()
 
 function returnDataFromDB(itemValue, databaseRef, dbArea){
     let newElement = document.createElement('li')
+    newElement.className = "database-list-item"
     newElement.textContent = itemValue[1]
     dbArea.appendChild(newElement)
 }
@@ -136,8 +176,17 @@ function addButtonClickListener(button, inputField, databaseRef){
 }
 
 
-
 addButtonClickListener(submitAppliedButton, submitAppliedInput, jobStatsInDB)
 addButtonClickListener(submitHeardBackButton, submitHeardBackInput, jobsHeardBackFromInDB)
-addButtonClickListener(submitAppliedButton, submitAppliedInput, jobsRejectedFromInDB)
-addButtonClickListener(submitAppliedButton, submitAppliedInput, jobsInterviewedForInDB)
+addButtonClickListener(submitRejectedButton, submitRejectedInput, jobsRejectedFromInDB)
+addButtonClickListener(submitInterviewedButton, submitInterviewedInput, jobsInterviewedForInDB)
+
+getCountFromDB(jobStatsInDB, jobsAppliedTo);
+getCountFromDB(jobsHeardBackFromInDB, jobsHeardBackFrom);
+getCountFromDB(jobsRejectedFromInDB, jobsRejectedFrom);
+getCountFromDB(jobsInterviewedForInDB, jobsInterviewedFor);
+
+toggleFormDisplay(appliedForm, appliedFormButton)
+toggleFormDisplay(heardBackForm, heardBackFormButton)
+toggleFormDisplay(rejectedForm, rejectedFormButton)
+toggleFormDisplay(interviewForm, interviewedFormButton)
